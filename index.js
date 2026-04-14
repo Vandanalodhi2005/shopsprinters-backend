66dc5c228d5daf2867875a02ae5bd0a81cfc7caf
unknown <vandanalodhi2005@gmail.com>
2026-04-15 04:29:33 +0530

    update email yaha se back krne pe vandana ka code vapis

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const io = new Server(server, {
 
 app.use(cors({
     origin: [
-        "https://printsmatrix1.netlify.app",
+        "https://printsmatrix.com",
         "http://localhost:5173",
         "http://localhost:5174",
         "https://innovationdynamicsgroup.com"
